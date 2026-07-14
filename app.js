23c355b5175c319220e9ab7a6a041fa3ccb67774
esemmelman <90999990+esemmelman@users.noreply.github.com>
2026-07-13 18:28:25 -0700

    v07

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APP_VERSION='v0.6';
+const APP_VERSION='v0.7';
 const STORAGE_KEY='notetree_pages_v1';
 let pages=JSON.parse(localStorage.getItem(STORAGE_KEY)||'[]');
 let currentPageId=null,newPageParentId=null,inlineNewParentId=null,contextPageId=null,renamePageId=null,draggedPageId=null,saveTimer=null;
